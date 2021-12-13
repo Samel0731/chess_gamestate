8f2e6b3c6cb43e7717c645634a12c3cc0453fd52
samel0731 <samel0731@gmail.com>
2021-12-13 20:05:36 +0800

    add lobby

--- states/lobby.py
+++ states/lobby.py
@@ -1,6 +1,196 @@
-import pygame
+from states.my_state import State
+import pygame, os, csv
+import json
 
-class Lobby():
-    def __init__(self):
-        pass
+class Lobby(State):
+    def __init__(self, game):
+        State.__init__(self,game)
+        self.spritesheet = Spritesheet(os.path.join(self.game.assets_dir, "lobby", "spritesheet.png"))
+        self.map = TileMap(os.path.join(self.game.assets_dir, "lobby", 'chessmap.csv'), self.spritesheet)
+        self.player = Player(self.game)
+        self.player.position.x, self.player.position.y = self.map.start_x, self.map.start_y
 
+    def render(self, display):
+        display.fill((0, 180, 240))
+        self.map.draw_map(display)
+        self.player.draw(display)
+
+    def update(self, delta_time, actions):
+        self.player.update(delta_time, self.map.tiles)
+        if actions['left']:
+            self.player.LEFT_KEY = True
+            self.player.RIGHT_KEY = False
+        if actions['right']:
+            self.player.LEFT_KEY = False
+            self.player.RIGHT_KEY = True
+        self.game.reset_keys()
+        
+        
+
+
+class Spritesheet:
+    def __init__(self, filename):
+        self.filename = filename
+        self.sprite_sheet = pygame.image.load(filename).convert()
+        self.meta_data = self.filename.replace('png', 'json')
+        with open(self.meta_data) as f:
+            self.data = json.load(f)
+        f.close()
+
+    def get_sprite(self, x, y, w, h):
+        sprite = pygame.Surface((w, h))
+        sprite.set_colorkey((0,0,0))
+        sprite.blit(self.sprite_sheet,(0, 0),(x, y, w, h))
+        return sprite
+
+    def parse_sprite(self, name):
+        sprite = self.data['frames'][name]['frame']
+        x, y, w, h = sprite["x"], sprite["y"], sprite["w"], sprite["h"]
+        image = self.get_sprite(x, y, w, h)
+        return image
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, spritesheet):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = spritesheet.parse_sprite(image)
+        # Manual load in: self.image = pygame.image.load(image)
+        self.image.set_colorkey((255,255,255))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+class TileMap():
+    def __init__(self, filename, spritesheet):
+        self.tile_size = 16
+        self.start_x, self.start_y = 0, 0
+        self.spritesheet = spritesheet
+        self.tiles = self.load_tiles(filename)
+        self.map_surface = pygame.Surface((self.map_w, self.map_h))
+        self.map_surface.set_colorkey((0, 0, 0))
+        self.load_map()
+
+    def draw_map(self, surface):
+        surface.blit(self.map_surface, (0, 0))
+
+    def load_map(self):
+        for tile in self.tiles:
+            tile.draw(self.map_surface)
+
+    def read_csv(self, filename):
+        map = []
+        with open(os.path.join(filename)) as data:
+            data = csv.reader(data, delimiter=',')
+            for row in data:
+                map.append(list(row))
+        return map
+
+    def load_tiles(self, filename):
+        tiles = []
+        map = self.read_csv(filename)
+        x, y = 0, 0
+        for row in map:
+            x = 0
+            for tile in row:
+                # if tile == '0':
+                    # self.start_x, self.start_y = x * self.tile_size, y * self.tile_size
+                # elif tile == '1':
+                    # tiles.append(Tile('grass.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
+                # elif tile == '2':
+                    # tiles.append(Tile('grass2.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
+                for png in range(136):
+                    if tile == str(png):
+                        tiles.append(Tile(f'{tile}.png', x * self.tile_size, y * self.tile_size, self.spritesheet))
+                    # Move to next tile in current row
+                x += 1
+
+            # Move to next row
+            y += 1
+            # Store the size of the tile map
+        self.map_w, self.map_h = x * self.tile_size, y * self.tile_size
+        return tiles
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game):
+        pygame.sprite.Sprite.__init__(self)
+        self.game = game
+        self.LEFT_KEY, self.RIGHT_KEY, self.FACING_LEFT = False, False, False
+        self.is_jumping, self.on_ground = False, False
+        self.gravity, self.friction = .35, -.12
+        self.sprite_dir = os.path.join(self.game.sprite_dir, "player")
+        self.image = pygame.image.load(os.path.join(self.sprite_dir, "player_right1.png")).convert()
+        self.rect = self.image.get_rect()
+        self.position, self.velocity = pygame.math.Vector2(0,0), pygame.math.Vector2(0,0)
+        self.acceleration = pygame.math.Vector2(0,self.gravity)
+
+    def draw(self, display):
+        display.blit(self.image, (self.rect.x, self.rect.y))
+
+    def update(self, dt, tiles):
+        dt = dt * 100
+        self.horizontal_movement(dt)
+        self.checkCollisionsx(tiles)
+        self.vertical_movement(dt)
+        self.checkCollisionsy(tiles)
+
+    def horizontal_movement(self,dt):
+        self.acceleration.x = 0
+        if self.LEFT_KEY:
+            self.acceleration.x -= .3
+        elif self.RIGHT_KEY:
+            self.acceleration.x += .3
+        self.acceleration.x += self.velocity.x * self.friction
+        self.velocity.x += self.acceleration.x * dt
+        self.limit_velocity(4)
+        self.position.x += self.velocity.x * dt + (self.acceleration.x * .5) * (dt * dt)
+        self.rect.x = self.position.x
+
+    def vertical_movement(self,dt):
+        self.velocity.y += self.acceleration.y * dt
+        if self.velocity.y > 7: self.velocity.y = 7
+        self.position.y += self.velocity.y * dt + (self.acceleration.y * .5) * (dt * dt)
+        self.rect.bottom = self.position.y
+
+    def limit_velocity(self, max_vel):
+        self.velocity.x = max(-max_vel, min(self.velocity.x, max_vel))
+        if abs(self.velocity.x) < .01: self.velocity.x = 0
+
+    def jump(self):
+        if self.on_ground:
+            self.is_jumping = True
+            self.velocity.y -= 8
+            self.on_ground = False
+
+    def get_hits(self, tiles):
+        hits = []
+        for tile in tiles:
+            if self.rect.colliderect(tile):
+                hits.append(tile)
+        return hits
+
+    def checkCollisionsx(self, tiles):
+        collisions = self.get_hits(tiles)
+        for tile in collisions:
+            if self.velocity.x > 0:  # Hit tile moving right
+                self.position.x = tile.rect.left - self.rect.w
+                self.rect.x = self.position.x
+            elif self.velocity.x < 0:  # Hit tile moving left
+                self.position.x = tile.rect.right
+                self.rect.x = self.position.x
+
+    def checkCollisionsy(self, tiles):
+        self.on_ground = False
+        self.rect.bottom += 1
+        collisions = self.get_hits(tiles)
+        for tile in collisions:
+            if self.velocity.y > 0:  # Hit tile from the top
+                self.on_ground = True
+                self.is_jumping = False
+                self.velocity.y = 0
+                self.position.y = tile.rect.top
+                self.rect.bottom = self.position.y
+            elif self.velocity.y < 0:  # Hit tile from the bottom
+                self.velocity.y = 0
+                self.position.y = tile.rect.bottom + self.rect.h
+                self.rect.bottom = self.position.y
